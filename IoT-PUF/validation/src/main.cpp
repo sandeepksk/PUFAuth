c174e929fcb580ea5c25d08dc28923fba5dbc6ab
sarat2225 <cs19btech11003@iith.ac.in>
2023-06-10 11:48:49 +0530

    updated validation code

--- IoT-PUF/validation/src/main.cpp
+++ IoT-PUF/validation/src/main.cpp
@@ -5,21 +5,47 @@
 #include <SRAM_CY62256N.h>
 #include <SPI.h>
 #include <SD.h>
-#include <Crypto.h>
-#include <SHA3.h>
 #include <string.h>
-#include <AES.h>
-#include <ArduinoJson.h>
 
 #define PIN_POWER_ANALOG A8
 #define PIN_POWER 9
 
 SRAM_CY62256N sram;
 
+String ssid = "DIR-615-9573";
+String password = "90013416";
+
+int identifier = 1;
+
+String serverIP = "192.168.0.187";  // IP address of the UDP server
+int serverPort = 18000;  // Port number of the UDP server
+
+
+struct auth_hdr {
+  uint8_t msgType;
+  uint32_t challenge1;
+  uint32_t challenge2;
+  uint64_t randomnumber;
+  uint32_t hash;
+  uint32_t identifier;
+  uint32_t prTime;
+} __attribute__((packed));
+
 
 boolean readBit(long location) {
   uint8_t result = sram.read(floor(location / 8));
   return result >> (7 - (location % 8)) & 0x1 == 1;
+}
+
+void connectToWiFi() {
+  Serial1.println("AT+CWJAP=\"" + ssid + "\",\"" + password + "\"");
+  delay(5000);
+
+  if (Serial1.find("OK")) {
+    Serial.println("Connected to Wi-Fi network: " + ssid);
+  } else {
+    Serial.println("Failed to connect to Wi-Fi network.");
+  }
 }
 
 void set() {
@@ -72,70 +98,11 @@ void initializeSD() {
 
 String* readSRAMfromMicroSD(String challenge1_id,String challenge2_id) {
 
-  String name = "CRPS.json";
+  String name = "CRPS.txt";
   String c1, c2;
 
   String r1="";
   String r2="";
-
-  File jsonFile = SD.open("CRPS.json"); // Replace "data.json" with your JSON file's name
-  if (!jsonFile) {
-    Serial.println("Failed to open JSON file.");
-    return;
-  }
-
-  StaticJsonDocument<bufferSize> jsonDocument;
-  DeserializationError error = deserializeJson(jsonDocument, jsonFile);
-  if (error) {
-    Serial.print("Failed to parse JSON file: ");
-    Serial.println(error.c_str());
-    return;
-  }
-
-  jsonFile.close();
-
-  // Retrieve challenges for a specific key
-  const char* challengesStr1 = jsonDocument[String(challenge1_id)]["challenges"];
-  const char* challengesStr2 = jsonDocument[String(challenge2_id)]["challenges"];
-
-  if (challengesStr1 && challengesStr2) 
-  {
-    // Split the challenges string by comma and convert each value to an integer
-    const char* delim = ", ";
-    char* challengeToken1 = strtok(const_cast<char*>(challengesStr1), delim);
-    char* challengeToken2 = strtok(const_cast<char*>(challengesStr2), delim);
-
-    while ((challengeToken1 != NULL) && (challengeToken2 != NULL)) {
-      c1 = atoi(challengeToken1);
-      c2 = atoi(challengeToken2);
-
-      if(readBit(c1))
-      {
-        r1 += "1";
-      }
-      else
-      {
-        r1 += "0";
-      }
-
-      if(readBit(c2))
-      {
-        r2 += "1";
-      }
-      else
-      {
-        r2 += "0";
-      }
-
-      challengeToken1 = strtok(NULL, delim);
-      challengeToken2 = strtok(NULL, delim);
-    }
-  }
-
-  else 
-  {
-    Serial.println("Invalid key index.");
-  }
 
   String out[2];
   out[0] = r1;
@@ -151,31 +118,98 @@ String hashCompute(String challenge1_id,String challenge2_id)
 
     turn_on_sram();
 
-    responses = readSRAMfromMicroSD(challenge1_id,challenge2_id);
+    // responses = readSRAMfromMicroSD(challenge1_id,challenge2_id);
 
     turn_off_sram();
-
-    Serial.println(responses[0]);
-    Serial.println(responses[1]);
   
+}
+
+// Function to convert a 32-bit value to network byte order
+uint32_t convertToNetworkByteOrder(uint32_t value) {
+  return ((value >> 24) & 0xFF) |
+         ((value >> 8) & 0xFF00) |
+         ((value << 8) & 0xFF0000) |
+         ((value << 24) & 0xFF000000);
+}
+
+bool sendRequest()
+{
+  auth_hdr auth_req;
+  
+  auth_req.msgType = 0x0;                     // Type is 0 for request packet
+  auth_req.identifier = convertToNetworkByteOrder(identifier);
+
+  // Calculate the total length of the header
+  size_t headerLength = sizeof(auth_hdr);
+
+  // Send the length of the header to the ESP8266
+  Serial1.print("AT+CIPSEND=1,");
+  Serial1.print(headerLength);
+  Serial1.print("\r\n");
+  delay(1000);
+
+  // Send each byte of the header using SoftwareSerial
+  uint8_t* headerBytes = reinterpret_cast<uint8_t*>(&auth_req);
+  for (size_t i = 0; i < headerLength; i++) {
+    Serial1.write(headerBytes[i]);
+}
+
+// Wait for the response from the ESP8266
+delay(1000);
 }
 
 void setup(void)
 {
   Serial.end();
   Serial.begin(115200);
+  Serial1.begin(115200);
   delay(1000);
 
-  String challenge1_id = "";
-  String challenge2_id = "";
   
   //setting ARDUNIO IO PINS
   set();
 
+  Serial.println("Set done");
+
   //intialising SD card
   initializeSD();
 
-  hashCompute(challenge1_id,challenge2_id);
+  Serial.println("Initializing ESP8266...");
+  Serial1.println("AT");
+  delay(1000);
+
+  if (Serial1.find("OK")) {
+    Serial.println("ESP8266 module is ready.");
+  } else {
+    Serial.println("Failed to initialize ESP8266 module.");
+    return;
+  }
+
+    // Connect to Wi-Fi network
+  connectToWiFi();
+
+  // Initialize UDP socket
+  Serial1.print("AT+CIPSTART=1,\"UDP\",\"");
+  Serial1.print(serverIP);
+  Serial1.print("\",");
+  Serial1.print(serverPort);
+  Serial1.print(",");
+  Serial1.print(serverPort);
+  Serial1.println(",0");
+  delay(1000);
+
+  if (Serial1.find("OK")) {
+    Serial.println("UDP socket initialized.");
+  } else {
+    Serial.println("Failed to initialize UDP socket.");
+    return;
+  }
+
+
+  //Initalising PUF protocol
+  Serial.println("Sending Request");
+  sendRequest();
+
 
 }
 
